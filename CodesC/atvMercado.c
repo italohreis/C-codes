a9303e566fa9d5cd7d9f20d76ddb8a1013081bc9
Italo <143908986+italohreis@users.noreply.github.com>
2023-10-24 22:28:47 -0300

    a

--- CodesC/atvMercado.c
+++ CodesC/atvMercado.c
@@ -11,6 +11,7 @@ quantidade em estoque, o programa deverá emitir uma mensagem de atendimento e d
 Do contrário, emitir mensagem, "Estoque com quantidade insuficiente para atender ao pedido". 
 O programa termina quando o número da linha for igual a -1.  */
 
+
 typedef struct{
     int qtdEstoque;
 } Produto;
@@ -19,67 +20,7 @@ typedef struct{
 } Armazem;
 Armazem armazens[6];
 
-int main(){             
-    printf("\n");
-    int armazem, produto, qtd;
-    //define random numbers for qtdEstoque;
-    srand(time(NULL));
-    for(armazem = 1; armazem <= 5; armazem++){
-        for (produto = 1; produto <= 5; produto++){
-            armazens[armazem].produtos[produto].qtdEstoque = rand() % 500;
-        }
-    }
-    
-    printArmazens();
 
-    start:
-    
-    printf("\n");
-    armazem = -1;
-
-    while(armazem < 1 || armazem > 5){
-        printf("\nQual armazem deseja alterar o estoque ? --> ");
-        scanf("%d", &armazem);
-            if(armazem == -1){
-                    return 0;
-            
-            } else if(armazem < 1 || armazem > 5){
-                printf(red_text"\nArmazem inexistente, tente novamente.\n" reset_color);
-            }
-    }
-    produto = -1;
-    while(produto < 1 || produto > 5){
-        printf("\nQual produto deseja alterar o estoque ? -->  ");
-        scanf("%d", &produto);
-        if(produto == -1){
-            return 0;
-
-        } else if(produto < 1 || produto > 5){
-            printf(red_text"\nProduto inexistente, tente novamente.\n" reset_color);
-        }
-    }
-    
-    printf("\nQual a quantidade a ser retirada do estoque ? --> ");
-    scanf("%d", &qtd);
-
-        if(qtd == -1){
-            printf("\n\nfim\n\n");
-            return 0;
-        }
-        
-    if(armazens[armazem].produtos[produto].qtdEstoque > qtd){
-        printf(green_text "\nAtendimento realizado com sucesso!\n\n" reset_color);
-        armazens[armazem].produtos[produto].qtdEstoque -= qtd;
-    }else{ 
-        printf(red_text "\nEstoque com quantidade insuficiente para atender ao pedido\n" reset_color);
-    }
-
-
-    printArmazens();
-
-    goto start;
-
-}
 
 void printArmazens(){
     //print table
@@ -93,3 +34,70 @@ void printArmazens(){
         }
     }
 }
+
+
+
+int main(){             
+    printf("\n");
+    int armazem, produto, qtd;
+    //define random numbers for qtdEstoque;
+    srand(time(NULL));
+    for(armazem = 1; armazem <= 5; armazem++){
+        for (produto = 1; produto <= 5; produto++){
+            armazens[armazem].produtos[produto].qtdEstoque = rand() % 500;
+        }
+    }
+    printf("\n\n* Caso queira finalizar, digite -1 *\n\n");
+    printArmazens();
+
+    start:
+    
+    printf("\n");
+    armazem = -1;
+
+    while(armazem < 1 || armazem > 5){
+        printf("\nQual armazem deseja alterar o estoque ? --> ");
+        scanf("%d", &armazem);
+            if(armazem == -1){
+                printf("\n\nfim\n\n");
+                return 0;
+
+            } else if(armazem < 1 || armazem > 5){
+                printf(red_text"\nArmazem inexistente, tente novamente.\n" reset_color);
+            }
+    }
+    produto = -1;
+    while(produto < 1 || produto > 5){
+        printf("\nQual produto deseja alterar o estoque ? -->  ");
+        scanf("%d", &produto);
+        if(produto == -1){
+            printf("\n\nfim\n\n");
+            return 0;
+
+        } else if(produto < 1 || produto > 5){
+            printf(red_text"\nProduto inexistente, tente novamente.\n" reset_color);
+        }
+    }
+    
+    
+    printf("\nQual a quantidade a ser retirada do estoque ? --> ");
+    scanf("%d", &qtd);
+
+        if(qtd == -1){
+            printf("\n\nfim\n\n");
+            return 0;
+        }
+            
+        if(armazens[armazem].produtos[produto].qtdEstoque > qtd){
+            printf(green_text "\nAtendimento realizado com sucesso!\n\n\n" reset_color);
+            armazens[armazem].produtos[produto].qtdEstoque -= qtd;
+        }else{ 
+            printf(red_text "\nEstoque com quantidade insuficiente para atender ao pedido\n" reset_color);
+        }
+
+    printArmazens();
+    goto start;
+
+    return 0;
+}
+
