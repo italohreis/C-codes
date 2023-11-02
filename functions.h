dadd9b65c8ac43430bb51efde33a1e46f1f37c60
Italo <143908986+italohreis@users.noreply.github.com>
2023-11-02 14:38:06 -0300

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,7 @@
+
+void relatorioDia();
+float relatorioPergunta(int cSim1, int cNao1, int cSim2, int cNao2);
+void relatorioCliente();
+float relatorioGeral(int somaClientes, int cSim2, int cClienteNao, int cEncProcurava5);
+
+
