7a86697dbef7f0cf82303c367e382b8d1df6d824
Italo <143908986+italohreis@users.noreply.github.com>
2023-11-08 14:28:58 +0000

    a

--- provas/n2Prova.c
+++ provas/n2Prova.c
@@ -1,254 +1,148 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-#define red_text "\x1b[31m"
-#define green_text "\x1b[32m"
-#define reset_color "\x1b[0m"
-#define orange_text "\x1b[33m"
+    /* Uma concessionária de veículos mantêm o cadastro de seus veículos desta forma:
+    código do veículo (número inteiro),
+    código da cor (número inteiro, 1 para branco, 2 para preto, 3 para vermelho e 4 para cinza),
+    quantidade.
+
+    A capacidade máxima de armazenagem nas suas garagens é de 150 veículos. Não permita que
+    sejam cadastrados mais veículos do que a capacidade.
+
+    Faça um programa que inicialmente leia os dados dos vários veículos que estão atualmente na
+    concessionária. Serão cadastrados, no máximo 25 tipos de veículos. Não permita código do veículo
+    com mesma cor em linhas diferentes.
+
+    Após esta leitura inicial, ofereça ao vendedor, sempre que desejar, a possibilidade de consultar um
+    destes relatórios: Relatório geral (mostre todos os veículos e seus dados, a quantidade total de
+    veículos e a percentagem de ocupação das garagens), relatório por cor (dada a cor, mostre a
+    quantidade total e todos os veículos desta cor), relatório por código (dado o código, mostre a
+    quantidade total e todos os veículos com o código e seus dados), busca por veículo (Dados o
+    código do veículo e sua cor, mostre a quantidade. Se o veículo for encontrado, a busca deve ser
+    interrompida). Pode ocorrer a necessidade de cadastrar um veículo que tenha sido adquirido
+    esporadicamente, então o programa deve ser capaz de inserir um veículo (apenas 1) a qualquer
+    momento. 
+ */
+
+    //prototipo das funcoes;
+    void RelatorioGeral();
+    void RelatorioCor();
+    void RelatorioCodigo();
+    void BuscaVeiculo();
+    void InserirVeiculo();
 
 
-    /* Uma escola de idiomas ministrará um curso intensivo de 1 ano. Serão abertas 2 turmas de no
-    máximo 5 alunos em cada. Os alunos recebem 3 notas ao longo do ano, a média aritmética destas
-    notas consiste na média final do aluno e a nota para receber o certificado é no mínimo 7,0 pontos. O
-    aluno deve ter também no máximo 10 faltas, caso contrário será reprovado por falta. Se o aluno
-    tiver média final abaixo de 5,0 também será reprovado. O aluno que não foi aprovado, nem
-    reprovado, terá a chance de fazer uma prova substitutiva. Elabore um programa em linguagem C
-    que leia para cada aluno, seu nome, suas 3 notas, sua frequência e mostre um dos os seguintes
-    relatórios:
-
-    - Consulta por aluno:
-    Dado o número do aluno e de sua turma, mostre seu nome, suas notas, sua média final, sua
-    frequência e uma das mensagens: “Reprovado por falta”, “Reprovado por nota”, “Certificado
-    Liberado” ou “Fará substitutiva”.
-
-    - Consulta por turma:
-    Dado o número da turma, mostre quantos alunos tem a turma, quantos tiveram média final acima de
-    8, quantos alunos tiveram algum zero em suas notas, quantos farão prova substitutiva e qual é a
-    média de notas geral da turma.
-
-    Após imprimir um relatório, deve ser possível ao usuário imprimir outro caso queira.
-    Obs.: Implementar pelo menos uma função que receba como parâmetros média final e frequência de
-    um aluno e retorne 0 se foi reprovado por falta, 1 se reprovado por nota, 2 se o certificado foi
-    liberado ou 3 se fará prova substitutiva */
-
-   int StatusAluno(int numTurma, int numAluno);
-
-    int frequencia[2][5];
-    float media[2][5];
 
 int main(void){
 
-    float Notas[2][5][3];
-    char nome[2][5][30];
-    int cAlunos[2] = {0};
-    int cMediaMaior8[2] = {0}, cSubst[2] = {0};
-    int cZero[2] = {0};
+    int numCodigo, numCor;
+    int carro[25][3];                   //carro[][0] --> codigo do carro                                
+    int car = 0;                        //carro[][1] --> cor do carro
+    int somaQuant = 0;               //carro[][2] --> quantidade                                  
+    for(car = 0; car < 25; car++){
+        printf("\nCarro %d\n", car);
 
-    int i, j, k;
-    for(i = 0; i < 2; i++){
-        for(j = 0; j < 5; j++){
-            for(k = 0; k < 3; k++){
-                Notas[i][j][k] = -1;
-            }
-        }
-    }
+        printf("informe o codigo do carro -> ");
+        scanf("%d", &carro[car][0]);                
 
-    float soma[2][5] = {{0}};
-    float somaGeral[2] = {0};
-    float MediaGeral[2] = {0};
-    int turma, aluno, nota;
+        printf("\ninforme a cor do veiculo (1 - branco / 2 - preto / 3 - vermelho / 4 - cinza) --> ");
+        scanf("%d", &carro[car][1]);
 
-    for(turma = 0; turma < 2; turma++){
-        printf("\nTurma %d\n", turma);
+        quant:
+        printf("\nInforme a quantidade --> ");
+        scanf("%d", &carro[car][2]);
 
-        for(aluno = 0; aluno < 5; aluno++){
-            cAlunos[turma]++;
-            printf("\n\t* Aluno %d *\n", aluno);
+        somaQuant += carro[car][2];
 
-            printf("\n\tDigite o nome do aluno --> ");
-            scanf("%s", &nome[turma][aluno]);
+        if(somaQuant > 150){
+            printf("\nQuantidade maxima atingida.\n");
 
-            printf("\n\tInforme a quantidade de faltas do aluno --> ");
-            scanf("%d", &frequencia[turma][aluno]);
+            somaQuant -= carro[car][2];
 
-            for(nota = 0; nota < 3; nota++){
-
-                while(Notas[turma][aluno][nota] < 0 || Notas[turma][aluno][nota] > 10){
-                    printf("\n\tDigite a nota %d do aluno --> ", nota);
-                    scanf("%f", &Notas[turma][aluno][nota]);
-
-                    if(Notas[turma][aluno][nota] < 0 || Notas[turma][aluno][nota] > 10){
-                        printf(red_text "\n\tNota invalida, tente novamente.\n" reset_color);
-                    }
-                }
-
-                soma[turma][aluno] += Notas[turma][aluno][nota];
-
-                if((frequencia[turma][aluno] < 10) && (Notas[turma][aluno][nota] > 5 && Notas[turma][aluno][nota] < 7)){
-                    cSubst[turma]++;
-                }
-
-            }
-            
-
-            media[turma][aluno] = soma[turma][aluno] / 3;
-            somaGeral[turma] += media[turma][aluno];
-
-            if(media[turma][aluno] > 8){
-                cMediaMaior8[turma]++;
-            }
-            
-
-            int resp = -1;
-            while(resp != 0 && resp != 1){
-                printf("\n\tDeseja continuar para outro aluno ? (1 - sim / 0 - nao) --> ");
-                scanf(" %d", &resp);
-
-                if(resp != 0 && resp != 1){
-                    printf(red_text "\n\tResposta invalida, tente novamente.\n" reset_color);
-
-                }
-            }
-
-            if(resp == 0){
-                break;
-            }
-            printf("\n");
+            goto quant;
         }
 
-            MediaGeral[turma] = (float) somaGeral[turma] / (float) cAlunos[turma];
+        printf("quant -> %d", somaQuant);
+
+
+
+        int resp;
+        printf("\nDeseja adicionar outro veiculo ? (1 - sim / 0 nao) --> ");
+        scanf("%d", &resp);
+
+        if(resp == 0){
+            break;
+        }
+
+
+
+
     }
+
+
+
+    int op;
+    printf("\nO que deseja fazer ?\n");
+    printf("1 - relatorio geral.\n 2 - relatorio por cor.\n 3 - relatorio por codigo.\n 4 - busca por veiculo.\n 5 - inserir um veiculo.\n 6 - finalizar.");
+    scanf("%d", &op);
+
+
 
     while(1){
-        int op;
-        int numAluno, numTurma;
-
-        printf("\n\nO que deseja fazer ?\n");
-        printf(" 1 - Consulta por aluno\n 2 - Consulta por turma.\n 3 - Finalizar.\n");
-        scanf("%d", &op);
 
         switch(op){
 
             case 1:
-                numTurma = -1;
-                while(numTurma != 0 && numTurma != 1){
-                    printf("\nQual turma pertence o aluno ? --> ");
-                    scanf(" %d", &numTurma);
-
-                    if(numTurma != 0 && numTurma != 1){
-                        printf(red_text "\nTurma invalida, tente novamente.\n" reset_color);
-                    }
-                }
-
-                numAluno = 6;
-                while(numAluno > cAlunos[numTurma] - 1){
-                    printf("\nQual aluno deseja consultar ? --> ");
-                    scanf("%d", &numAluno);
-
-                    if(numAluno > cAlunos[numTurma] - 1){
-                        printf(red_text "\nAluno inexistente, tente novamente.\n" reset_color);
-                    }
-                }
-
                 
-                StatusAluno(numTurma, numAluno);
-
-                printf("\n\tNome do aluno --> %s\n", nome[numTurma][numAluno]);
-
-                int i;
-                for(i = 0; i < 3; i++){
-                    printf("\n\tNota %d -->  %.2f\n", i, Notas[numTurma][numAluno][i]);
-                }
-
-                printf("\n\tMedia final do aluno -->  %.2f\n", media[numTurma][numAluno]);
-
-                printf("\n\tQuantidade de faltas do aluno --> %d\n", frequencia[numTurma][numAluno]);
-
-                int numero;
-                numero = StatusAluno(numTurma, numAluno);
-
-                if(numero == 0){
-                    printf(red_text "\n\tReprovado por falta.\n" reset_color);
-
-                } else if(numero == 1){
-                    printf(red_text "\n\tReprovado por nota.\n" reset_color);
-
-                } else if (numero == 2){
-                    printf(green_text "\n\tCertificado liberado.\n" reset_color);
-
-                } else{
-                    printf(orange_text "\n\tFara prova substitutiva.\n" reset_color);
-                }
-                break;
 
             case 2:
-                numTurma = -1;
-                while(numTurma != 0 && numTurma != 1){
-                    printf("\nQual turma deseja consultar ? --> ");
-                    scanf("%d", &numTurma);
-
-                    if(numTurma != 0 && numTurma != 1){
-                        printf(red_text "\nTurma invalida, tente novamente.\n" reset_color);
-                    }
-                }
-
-                printf("\n\tQuantidade de alunos na turma %d -->  %d\n", numTurma, cAlunos[numTurma]);
-
-                printf("\n\tQuantidade de alunos com media final maior que 8 -->  %d\n", cMediaMaior8[numTurma]);
-
-                int k, j;
-                for(k = 0; k < cAlunos[numTurma]; k++){
-                    for(j = 0; j < 3; j++){
-                        if(Notas[numTurma][k][j] == 0){
-                            cZero[numTurma]++;
-                            break;
-                        }
-                    }
-                }
-
-                printf("\n\tQuantidade de alunos que tiverem alguma nota com 0 -->  %d\n", cZero[numTurma]);
-
-                printf("\n\tQuantidade de alunos que farao prova substitutiva --> %d\n", cSubst[numTurma]);
-
-                printf("\n\tMedia geral da turma -->  %.2f\n", MediaGeral[numTurma]);
+                printf("\nQual cor deseja consultar ? --> ");
+                scanf("%d", &numCor);
                 break;
+                
 
             case 3:
-                printf("\n\nfim.\n\n");
+                
+                printf("\nQual codigo do veiculo que deseja consultar ? --> ");
+                scanf("%d", &numCodigo);
+
+               
+                
+                break;
+
+            case 4:
+
+                printf("Qual o codigo do veiculo ? --> ");
+                scanf("%d", &numCodigo);
+
+                int numCor;
+                printf("\nQual cor do veiculo ? --> ");
+                scanf("%d", &numCor);
+                break;
+                
+
+            case 5:
+                printf("\nqual codigo do veiculo ? --> ");
+                scanf("%d", &numCodigo);
+
+                printf("\nqual cor do veiculo --> ");
+                scanf("%d", &numCor);
+                break;
+
+            case 6:
+                printf("\nfim.\n");
                 return 0;
+                break;
 
             default:
-                printf(red_text "\nResposta invalida, tente novamente.\n" reset_color);
-                break;
+                printf("\nResposta invalida.\n");
+
 
 
         }
     }
+
 
 
     return 0;
-}
-
-
-
-
-int StatusAluno(int numTurma, int numAluno){
-
-    if (frequencia[numTurma][numAluno] > 10){
-        return 0;
-
-    } else{
-
-        if(media[numTurma][numAluno] < 5){
-            return 1;
-
-        } else if(media[numTurma][numAluno] >= 7){
-            return 2;
-
-        } else{
-            return 3;
-        }
-    }
 
 }
