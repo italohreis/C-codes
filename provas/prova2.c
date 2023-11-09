a8bf36f7a0eeaa3caa97910148cd756bba578ef6
Italo <143908986+italohreis@users.noreply.github.com>
2023-11-09 15:31:39 +0000

    a

--- provas/prova2.c
+++ provas/prova2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 #define red_text "\x1b[31m"
+#define green_text "\x1b[32m"
 #define reset_color "\x1b[0m"
 
 
@@ -35,52 +35,76 @@ typedef struct {
     int cor;
     int quantidadeCarro;
 } carro;
+    carro carros[25];
 
-carro carros[25];
+//prototipos das funcoes
 
-void InserirCarros(int code, int color, int *QuantidadeTotal);
+void QuantidadeCarros(int code, int color, int *QuantidadeTotal, int *cCarros);
 
 void RelatorioGeral();
 void RelatorioCor(int color);
 void RelatorioCodigo(int code);
-void BuscaVeiculo(int code, int color);
+void BuscaVeiculo(int code, int color, int cCarros);
 void InserirVeiculo(int code, int color, int *QuantidadeTotal);
 
-int main(void) {
-    int QuantidadeTotal = 0;
-    for (int i = 0; i < 25; i++) {
+
+int main(void){
+    for (int i = 0; i < 25; i++){
         carros[i].codigo = 0;
     }
 
+    int QuantidadeTotal = 0;
     int numCor, numCodigo;
     int code, color;
-    int car;
     bool opcao = false;
+    int resp, cCarros = 0;     //cCarros -> conta a quantidade de tipos de carros inseridos
 
-
-    for (car = 0; car < 25; car++) {
+    do{
+        printf("\n---------------------------------------------------------------------------------\n");
+        printf("\nCarro %d\n", cCarros);
         printf("\nDigite o codigo do carro --> ");
         scanf("%d", &code);
 
+        color = 0;
+        while(color < 1 || color > 4){          //estrutura de repeticao para verificar a resposta do usuario
+            printf("\nDigite a cor do carro (1 - branco / 2 - preto / 3 - vermelho / 4 - cinza) --> ");
+            scanf("%d", &color);
 
-        printf("\nDigite a cor do carro (1 - branco / 2 - preto / 3 - vermelho / 4 - cinza) --> ");
-        scanf("%d", &color);
+            if(color < 1 || color > 4){
+                printf(red_text "\nCor invalida, tente novamente.\n" reset_color);
+            }
+        }
 
-        InserirCarros(code, color, &QuantidadeTotal);
+        QuantidadeCarros(code, color, &QuantidadeTotal, &cCarros);  //Função para inserir a quantidade de carros.
 
-        printf("Deseja inserir mais carros ? (1 - sim / 0 - nao) --> ");
-        int resp;
-        scanf("%d", &resp);
+        resp = -1;
+        while(resp != 0 && resp != 1){
+            printf("\nDeseja inserir mais carros ? (1 - sim / 0 - nao) --> ");
+            scanf("%d", &resp);
 
-        if (resp == 0) {
+            if(resp != 0 && resp != 1){
+                printf(red_text "\nResposta invalida, tente novamente.\n" reset_color);
+            }
+        }                                                 
+
+        if (resp == 0){
             break;
         }
-    }
+        printf("carros -> %d\n", cCarros);
+        if(cCarros == 25){
+            printf(red_text "\nLimite de carros atingido.\n" reset_color);
+            break;
+        }
+
+    } while (resp == 1);
+
 
     int op;
-    while (1) {
+    while (1){
+
         printf("\n\nO que deseja fazer ?\n\n");
-        printf(" 1 - relatorio geral.\n 2 - relatorio por cor.\n 3 - relatorio por codigo.\n 4 - busca por veiculo.\n 5 - inserir um veiculo.\n 6 - finalizar.\n");
+        printf(" 1 - relatorio geral.\n 2 - relatorio por cor.\n 3 - relatorio por codigo.\n 4 - busca por veiculo.\n" 
+        " 5 - inserir um veiculo.\n 6 - finalizar.\n");
         scanf("%d", &op);
 
         switch (op){
@@ -110,28 +134,44 @@ int main(void) {
                 printf("\nQual cor do veiculo ? --> ");
                 scanf("%d", &numCor);
                 
-                BuscaVeiculo(numCodigo, numCor);
+                BuscaVeiculo(numCodigo, numCor, cCarros);
 
                 break;
 
             case 5:
-
                 if(opcao == true){
                     printf(red_text "\nVeiculo ja adicionado.\n" reset_color);
                     break;
                 }
-                printf("\nqual codigo do veiculo ? --> ");
+
+                printf("\nQual o codigo do veiculo ? --> ");
                 scanf("%d", &numCodigo);
-                printf("\nqual cor do veiculo --> ");
+
+                printf("\nQual cor do veiculo ? --> ");
                 scanf("%d", &numCor);
 
-                InserirCarros(numCodigo, numCor, &QuantidadeTotal); //porque envia como parametro o enderço de memoria da variavel QuantidadeTotal?
+                for(int i = 0; i < 25; i++){
+                    if(carros[i].codigo == numCodigo && carros[i].cor == numCor){
+                        carros[i].quantidadeCarro += 1;
+                        QuantidadeTotal += 1;
+                        break;
+                    }
+
+                    if(carros[i].codigo == 0){
+                        carros[i].codigo = numCodigo;
+                        carros[i].cor = numCor;
+                        carros[i].quantidadeCarro = 1;
+                        QuantidadeTotal += 1;
+
+                        printf(green_text "\nVeiculo inserido com sucesso.\n" reset_color);
+                        break;
+                    }
+
+                }
 
                 opcao = true;
+                
                 break;
-
-
-
             case 6:
                 printf("\nfim.\n");
                 return 0;
@@ -144,37 +184,48 @@ int main(void) {
     return 0;
 }
 
-void InserirCarros(int code, int color, int *QuantidadeTotal){
+
+
+
+void QuantidadeCarros(int code, int color, int *QuantidadeTotal, int *cCarros){
     int quantidade;
     printf("\nDigite a quantidade de carros --> ");
     scanf("%d", &quantidade);
-    for (int i = 0; i < 25; i++) {
+
+    for (int i = 0; i < 25; i++){
         if (code == carros[i].codigo && color == carros[i].cor) {
             carros[i].quantidadeCarro += quantidade;
             *QuantidadeTotal += quantidade;
-            printf("\nquantidade de carros: %d\n", carros[i].quantidadeCarro);
-            return; // Saia da função após a atualização.
-        }
-
-        if (carros[i].codigo == 0){
-
-            carros[i].codigo = code;
-            carros[i].cor = color;
-            carros[i].quantidadeCarro = quantidade;
-            *QuantidadeTotal += quantidade;
-
-            printf("\nQuantidade total de carros: %d\n", *QuantidadeTotal);
 
             if (*QuantidadeTotal > 150){
                 printf(red_text "\nLimite de carros atingido.\n" reset_color);
 
                 *QuantidadeTotal -= quantidade;
 
+                carros[i].quantidadeCarro -= quantidade;
+
+            }
+            printf("Quant -> %d\n", carros[i].quantidadeCarro);
+            return; 
+        }
+
+        if (carros[i].codigo == 0){
+            *cCarros += 1;
+            carros[i].codigo = code;
+            carros[i].cor = color;
+            carros[i].quantidadeCarro = quantidade;
+            *QuantidadeTotal += quantidade;
+
+
+            if (*QuantidadeTotal > 150){
+                printf(red_text "\nLimite de carros atingido.\n" reset_color);
+
+                *QuantidadeTotal -= quantidade;
+                *cCarros -= 1;
+
                 carros[i].codigo = 0;
                 carros[i].cor = 0;
                 carros[i].quantidadeCarro = 0;
-
-                printf("\nQuantidade total de carros: %d\n", *QuantidadeTotal);
 
             }
             return; // Saia da função após a inserção.
@@ -184,7 +235,7 @@ void InserirCarros(int code, int color, int *QuantidadeTotal){
 
 void RelatorioGeral(int QuantidadeTotal){
     float porcentagem;
-    printf("\n\tRelatorio geral:\n");
+    printf("\n\t< Relatorio geral >\n");
     printf("\n\tQuantidade total de carros: %d\n", QuantidadeTotal);
 
     for (int i = 0; i < 25; i++) {
@@ -211,33 +262,33 @@ void RelatorioGeral(int QuantidadeTotal){
     }
     porcentagem = ((float) QuantidadeTotal * 100) / 150;
 
-    printf("\n\tPorcentagem de ocupacao das garagens --> %.2f%%\n", porcentagem);
+    printf("\n\tPorcentagem de ocupacao da garagem --> %.2f%%\n", porcentagem);
     return;
 }
 
 void RelatorioCor(int color){
     int quantidade = 0;
     printf("\n< Relatorio por cor >\n");
+    printf("\n\tCor dos carros --> ");
+    switch(color){
+        case 1:
+            printf("Branco\n");
+            break;
+        case 2:
+            printf("Preto\n");
+            break;
+        case 3:
+            printf("Vermelho\n");
+            break;
+        case 4:
+            printf("Cinza\n");
+            break;
+    }
 
     for (int i = 0; i < 25; i++) {
-        if (carros[i].cor == color) {
+        if (carros[i].cor == color){
             printf("\n\tCodigo do carro: %d\n", carros[i].codigo);
-            printf("\tCor do carro: --> ");
 
-            switch (carros[i].cor){
-                case 1:
-                    printf("Branco\n");
-                    break;
-                case 2:
-                    printf("Preto\n");
-                    break;
-                case 3:
-                    printf("Vermelho\n");
-                    break;
-                case 4:
-                    printf("Cinza\n");
-                    break;
-            }
             quantidade += carros[i].quantidadeCarro;
         }
     }
@@ -275,13 +326,18 @@ void RelatorioCodigo(int code){
     return;
 }
 
-void BuscaVeiculo(int code, int color){
+void BuscaVeiculo(int code, int color, int cCarros){
     int i;
 
-    for(i = 0; i < 25; i++){
+    for(i = 0; i < cCarros; i++){
         if((carros[i].codigo == code) && (carros[i].cor == color)){
             
             printf("\n\tQuantidade de veiculos --> %d\n", carros[i].quantidadeCarro);
+            return;
+        } 
+
+        if(carros[i].codigo != code && carros[i].cor != color){       
+            printf(red_text "\nVeiculo nao encontrado.\n" reset_color);
             return;
         }
     }
