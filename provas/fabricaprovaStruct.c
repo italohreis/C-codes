0a01900592deb1efae4153f502a0b09281597030
Italo <143908986+italohreis@users.noreply.github.com>
2023-11-24 17:47:49 +0000

    a

--- provas/fabricaprovaStruct.c
+++ provas/fabricaprovaStruct.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-
-    #define red_text "\x1b[31m"
-    #define reset_color "\x1b[0m"
+#define red_text "\x1b[31m"
+#define reset_color "\x1b[0m"
 
  // Uma fábrica de argamassas vende 3 produtos: argamassa AC3 20kg, argamassa AC2 20kg e rejunte
     // 5kg. Suas entregas são feitas em Palmas ou Porto Nacional. Durante o último ano, esta fábrica
@@ -37,6 +36,7 @@ typedef struct{
 typedef struct{
     pedido pedidos[10];
     float totalKgCliente;
+    int cPedidos;
 } cliente;
 
     //prototipo
@@ -48,51 +48,82 @@ typedef struct{
 
 int main(){
     cliente clientes[2];
+    //zerar struct
+    int i, j;
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 10; j++){
+            clientes[i].pedidos[j].tipo = 0;
+            clientes[i].pedidos[j].quantidade = 0;
+            clientes[i].pedidos[j].local = 0;
+            clientes[i].pedidos[j].pesoPedido = 0;
+        }
+    }
 
     int totalkg = 0, cPedidos = 0;
     int cliente, resp;
     int cPedidosMenor1000 = 0;
 
     for(cliente = 0; cliente < 2; cliente++){
+        clientes[cliente].cPedidos = 0;
+        clientes[cliente].totalKgCliente = 0;
         printf("\n* Cliente %d *\n", cliente);
 
         int request;
         for(request = 0; request < 10; request++){
             printf("\n\t< Pedido %d >\n", request);
 
-            printf("\n\tInforme o tipo de produto (1 - Argamassa AC3 20kg / 2 - Argamassa AC2 20kg / 3 - Rejunte 5kg) --> ");
-            scanf("%d", &clientes[cliente].pedidos[request].tipo);
+            while(clientes[cliente].pedidos[request].tipo < 1 || clientes[cliente].pedidos[request].tipo > 3){
+                printf("\n\tInforme o tipo de produto (1 - Argamassa AC3 20kg / 2 - Argamassa AC2 20kg / 3 - Rejunte 5kg) --> ");
+                scanf("%d", &clientes[cliente].pedidos[request].tipo);
 
-            printf("\n\tInforme o local de entrega do pedido (1 - Palmas / 2 - Porto Nacional) --> ");
-            scanf("%d", &clientes[cliente].pedidos[request].local);
+                if(clientes[cliente].pedidos[request].tipo < 1 || clientes[cliente].pedidos[request].tipo > 3){
+                    printf(red_text "\n\tTipo de produto invalido, tente novamente.\n" reset_color);
+                }
+            }
+
+            while(clientes[cliente].pedidos[request].local != 1 && clientes[cliente].pedidos[request].local != 2){
+                printf("\n\tInforme o local de entrega do pedido (1 - Palmas / 2 - Porto Nacional) --> ");
+                scanf("%d", &clientes[cliente].pedidos[request].local);
+
+                if(clientes[cliente].pedidos[request].local != 1 && clientes[cliente].pedidos[request].local != 2){
+                    printf(red_text "\n\tLocal de entrega invalido, tente novamente.\n" reset_color);
+                }
+            }
 
             printf("\n\tInforme a quantidade de pacotes do pedido --> ");
             scanf("%d", &clientes[cliente].pedidos[request].quantidade);
 
 
-            if(clientes[cliente].pedidos[request].tipo == 1){
+            if(clientes[cliente].pedidos[request].tipo == 1 || clientes[cliente].pedidos[request].tipo == 2){
                 clientes[cliente].pedidos[request].pesoPedido = clientes[cliente].pedidos[request].quantidade * 20;
 
-            } else if(clientes[cliente].pedidos[cPedidos].tipo == 2){
-                clientes[cliente].pedidos[request].pesoPedido = clientes[cliente].pedidos[request].quantidade * 20;
-              
             } else{
                 clientes[cliente].pedidos[request].pesoPedido = clientes[cliente].pedidos[request].quantidade * 5;
             }
 
-            if(clientes[cliente].pedidos[request].pesoPedido){
+
+            if(clientes[cliente].pedidos[request].pesoPedido < 1000){
                 cPedidosMenor1000++;
             }
 
             totalkg += clientes[cliente].pedidos[request].pesoPedido;
+            clientes[cliente].totalKgCliente += clientes[cliente].pedidos[request].pesoPedido;            
 
-            printf("\n\tDeseja continuar para outro pedido ? (1 - sim / 0 - nao) --> ");
-            scanf("%d", &resp);
+            clientes[cliente].cPedidos++;
 
-            if(resp == 0){
-                break;
+            resp = -1;
+            while(resp != 1 && resp != 0){
+                printf("\n\tDeseja continuar para outro pedido ? (1 - sim / 0 - nao) --> ");
+                scanf("%d", &resp);
+
+                if(resp != 1 && resp != 0){
+                    printf(red_text "\n\tResposta invalida, tente novamente.\n" reset_color);
+
+                } else if(resp == 0){
+                    break;
+                }
+                
             }
-
 
         }
     }
@@ -100,7 +131,7 @@ int main(){
     while(1){
 
         int op;
-        printf("\nO que deseja fazer ?\n");
+        printf("\n\n\nO que deseja fazer ?\n");
         printf("\n 1 - Relatorio por pedido.\n 2 - Relatorio por cliente.\n 3 - Relatorio Geral.\n 4 - Finalizar.\n");
         scanf("%d", &op);
 
@@ -134,14 +165,29 @@ int main(){
 
 void RelatorioPedido(cliente *clientes){
 
-    int numCliente, numPedido;
-    printf("\nQual cliente deseja consultar ? --> ");
-    scanf("%d", &numCliente);
+    int numCliente = -1, numPedido;
+    while(numCliente < 0 || numCliente > 1){
+        printf("\nQual cliente deseja consultar ? --> ");
+        scanf("%d", &numCliente);
 
-    printf("\nQual pedido deseja consultar ? --> ");
-    scanf("%d", &numPedido);
+        if(numCliente < 0 || numCliente > 1){
+            printf(red_text "\nCliente invalido, tente novamente.\n" reset_color);
+        }
+    }
 
-    printf("\nTipo de produto --> ");
+    while(numPedido < 0 || numPedido >= clientes[numCliente].cPedidos){
+        printf("\nQual pedido deseja consultar ? --> ");
+        scanf("%d", &numPedido);
+
+        if(numPedido < 0 || numPedido >= clientes[numCliente].cPedidos){
+            printf(red_text "\nPedido invalido, tente novamente.\n" reset_color);
+        }
+    }
+
+    printf("\n\t-------------------------------------------------\n");
+    
+
+    printf("\n\tTipo de produto --> ");
 
     switch(clientes[numCliente].pedidos[numPedido].tipo){
         case 1:
@@ -157,9 +203,10 @@ void RelatorioPedido(cliente *clientes){
             break;
     }
 
-    printf("\nQuantidade de pacotes --> %d\n", clientes[numCliente].pedidos[numPedido].quantidade);
+    printf("\n\tQuantidade de pacotes --> %d\n", clientes[numCliente].pedidos[numPedido].quantidade);
 
-    printf("\nLocal de entrega --> ");
+    printf("\n\tLocal de entrega --> ");
+
 
     switch(clientes[numCliente].pedidos[numPedido].local){
         case 1:
@@ -171,36 +218,42 @@ void RelatorioPedido(cliente *clientes){
             break;
     }
 
-    printf("\nValor do frete --> R$ %.2f\n", CalcularFrete(clientes, numCliente, numPedido));
+    printf("\n\tValor do frete --> R$ %.2f\n", CalcularFrete(clientes, numCliente, numPedido));
+    printf("\n\t-------------------------------------------------\n");
 
     return;
 }
-
+ 
 
 void RelatorioCliente(cliente *clientes){
-    int numCliente, qPedidos;
-    printf("\nQual cliente deseja consultar ? --> ");
-    scanf("%d", &numCliente);
+    int numCliente = -1;
 
-    int i;
-    for(i = 0; i < 10; i++){
-        if(clientes[numCliente].pedidos[i].tipo > 0){
-            qPedidos++;
+    while(numCliente < 0 || numCliente > 1){
+        printf("\nQual cliente deseja consultar ? --> ");
+        scanf("%d", &numCliente);
+
+        if(numCliente < 0 || numCliente > 1){
+            printf(red_text "\nCliente invalido, tente novamente.\n" reset_color);
         }
     }
 
-    printf("\nMedia de peso dos pedidos --> %.2f\n", clientes[numCliente].totalKgCliente / qPedidos);
+    printf("\n\t-------------------------------------------------\n");
+    printf("\n\tMedia de peso dos pedidos --> %.2f\n", clientes[numCliente].totalKgCliente / (float)clientes[numCliente].cPedidos);
 
-    printf("\nQuantidade de pedidos feitos no periodo --> %d\n", qPedidos);
+    printf("\n\tQuantidade de pedidos feitos pelo cliente --> %d\n", clientes[numCliente].cPedidos);
+    printf("\n\t-------------------------------------------------\n");
 
     return;
 }
 
 void RelatorioGeral(int totalkg, int cPedidosMenor1000){
 
-    printf("\nQuantidade total em kg de material vendido --> %d\n", totalkg);
+    printf("\n\t---------------------------------------------------------------\n");
 
-    printf("\nQuantidade de pedidos que possuem menos que 1000kg --> %d\n", cPedidosMenor1000);
+    printf("\n\tQuantidade total em kg de material vendido --> %d\n", totalkg);
+
+    printf("\n\tQuantidade de pedidos que possuem menos que 1000kg --> %d\n", cPedidosMenor1000);
+    printf("\n\t---------------------------------------------------------------\n");
 
     return;
 }
@@ -225,8 +278,4 @@ float CalcularFrete(cliente *clientes, int numCliente, int numPedido){
 
         return frete;
     }
-
-
 }
-
-
