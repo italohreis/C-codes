935120916adc9e4c1d89ff286f78e664872acc18
Italo <143908986+italohreis@users.noreply.github.com>
2023-12-05 20:10:15 -0300

    Delete provas directory

--- provas/provasAntigas/provan2/provan2/functions.h
+++
@@ -1,7 +0,0 @@
-
-void relatorioDia();
-float relatorioPergunta(int cSim1, int cNao1, int cSim2, int cNao2);
-void relatorioCliente();
-float relatorioGeral(int somaClientes, int cSim2, int cClienteNao, int cEncProcurava5);
-
-
