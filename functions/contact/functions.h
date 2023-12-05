e432fb9ad623de2d001f03a3992cc1c6476a8b19
Italo <143908986+italohreis@users.noreply.github.com>
2023-12-05 20:09:43 -0300

    Delete functions directory

--- functions/contact/functions.h
+++
@@ -1,5 +0,0 @@
-void AddContact(int *cContacts);
-void EditContact(int *cContacts);
-void DeleteContact(int *cContacts);
-void ConsultContact(int *cContacts);
-void ConsultAllContacts(int *cContacts);
